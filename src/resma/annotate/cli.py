1850b2b2aa3bdf1319e2543647a712a95618f896
Christian Camacho <chris.ca.dev@gmail.com>
2025-06-25 18:36:37 -0700

    feat(annotate): add shortcut to add not quickly

--- src/resma/annotate/cli.py
+++ src/resma/annotate/cli.py
@@ -1,4 +1,5 @@
-from click import argument, option, group, STRING, pass_context
+from typing import Union
+from click import Context, argument, option, group, STRING, pass_context
 import click
 from click_aliases import ClickAliasedGroup
 
@@ -8,10 +9,16 @@ from resma.annotate.interfaces.factories.environment_factory import make_annotat
 env = make_annotate_environment()
 
 
-@group(cls=ClickAliasedGroup)
+@group(cls=ClickAliasedGroup, invoke_without_command=True)
 @pass_context
-def main(ctx):
-    pass
+@option('--name', '-n', type=STRING)
+@option("--vault", "-v", type=STRING, default=env.default_vault)
+def main(ctx: Context, name: Union[str, None] = None, vault: Union[str, None] = None):
+    if not name:
+        click.UsageError("Note name is required", ctx)
+    if not ctx.invoked_subcommand:
+        ctx.invoke(add_note, name=name, vault=vault, quiet=True)
+        ctx.invoke(edit_note, name=name, vault=vault, quiet=False)
 
 
 @main.command(aliases=("add", "an", "a"))
@@ -29,14 +36,16 @@ def main(ctx):
     type=STRING,
     help="Template shortcut name",
 )
-def add_note(ctx, name: str, vault: str, template: str):
+@option("--quiet", "-q", is_flag=True, default=False)
+def add_note(ctx, name: str, vault: str, template: str = None, quiet: bool = False):
     controller = make_create_note_controller(ctx.obj.get('env'))
     note = controller.create_note(
         name=name,
         vault_name=vault,
         template=template,
     )
-    click.echo(note.filepath)
+    if not quiet:
+        click.echo(note.filepath)
 
 
 @main.command(aliases=("edit", "en", "e"))
@@ -47,11 +56,13 @@ def add_note(ctx, name: str, vault: str, template: str):
     default=env.default_vault,
     help="Vault name or directory path"
 )
-def edit_note(ctx, name: str, vault: str):
+@option("--quiet", "-q", is_flag=True, default=False)
+def edit_note(ctx, name: str, vault: str, quiet: bool = False):
     dto = make_edit_note_controller(
         ctx.obj.get('env')
     ).edit_note(
         name=name,
         vault=vault,
     )
-    click.echo(dto.note.filepath)
+    if not quiet:
+        click.echo(dto.note.filepath)
